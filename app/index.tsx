8a68c841b9f78bdb23305e0e079c7aa0c3a754e5
guts203 <david.baehler03@gmail.com>
2024-10-03 19:01:45 +0200

    recupération des pokémons venant de l'api

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,37 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Image,Text, View, StyleSheet, ActivityIndicator } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedText } from "@/components/ThemedText";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { Card } from "@/components/Card";
+import { FlatList } from "react-native";
+import { PokemonCard } from "@/components/pokemon/PokemonCard";
+import { useFetchQuery } from "@/hooks/useFetchQuery";
+import { getPokemonId } from "@/functions/pokemon";
 
 export default function Index() {
+  const colors=useThemeColors();
+  const {data, isFetching}=useFetchQuery('/pokemon?limit=21')
+  const pokemons=data?.results ?? []
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/about">À propos</Link>
-      <Link href={{ pathname: '/pokemon/[id]', params: { id: 3 } }}>Pokemon 2</Link>
+    <SafeAreaView style={[styles.container,{backgroundColor:colors.tint}]}>
+      <View style={styles.header}>
+        <Image source={require("@/app/assets/images/Pokeball.png")} width={24} height={24}/>
+        <ThemedText variant="headline" color="grayLight">Pokédex</ThemedText>
+      </View>
+      <Card style={styles.body}>
+        <FlatList 
+            data={pokemons} 
+            numColumns={3}
+            contentContainerStyle={[styles.gridGap, styles.list]}
+            columnWrapperStyle={styles.gridGap}
+            ListFooterComponent={
+              isFetching ? <ActivityIndicator color={colors.tint}/>:null
+            }
+            renderItem={({item})=><PokemonCard id={getPokemonId(item.url)} name={item.name} style={{flex:1/3}}/>} 
+            keyExtractor={(item)=>item.url}/>
+      </Card>
+      
 
       {/* Utilisation correcte de pathname et params */}
       
@@ -17,7 +41,22 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FF0000',
-    
+    flex:1,
+    padding:4,
   },
+  header:{
+    flexDirection:'row',
+    alignItems:'center',
+    gap:16,
+    padding:12,
+  },
+  body:{
+    flex:1,
+  },
+  gridGap:{
+    gap:8,
+  },
+  list:{
+    padding:12,
+  }
 });
